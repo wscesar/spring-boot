9388e5099d15ad8239067f9e1f8ce8d8dbd9ef90
William Siqueira <wscesar@gmail.com>
2020-06-18 12:58:35 -0300

    Created Fake Database

---
+++ src/main/java/com/wscesar/dao/FakeDataDao.java
@@ -0,0 +1,45 @@
+package com.wscesar.dao;
+
+import com.wscesar.model.User;
+import com.wscesar.model.User.Gender;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class FakeDataDao implements UserDao {
+
+  private static Map<UUID, User> database;
+
+  static {
+    database = new HashMap<>();
+    UUID userId = UUID.randomUUID();
+    database.put(
+        userId,
+        new User(userId, "john@email.com", "John", "Doe", Gender.MALE, 35)
+    );
+  }
+
+  @Override
+  public List<User> getAllUsers() {
+    return new ArrayList<>(database.values());
+  }
+
+  @Override
+  public User getUser(UUID userUid) {
+    return database.get(userUid);
+  }
+
+  @Override
+  public int updateUser(User user) {
+    database.put(user.getUuid(), user);
+    return 1;
+  }
+
+  @Override
+  public int removeUser(UUID userUid) {
+    database.remove(userUid);
+    return 1;
+  }
+}
